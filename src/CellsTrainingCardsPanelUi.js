083d20f976199554fcc7219b495cca1f5c1bd0dd
Daniel Cardona <cardonacalderondaniel@gmail.com>
2023-02-26 10:11:00 -0500

    Ajuste a los textos

--- src/CellsTrainingCardsPanelUi.js
+++ src/CellsTrainingCardsPanelUi.js
@@ -67,15 +67,70 @@ export class CellsTrainingCardsPanelUi extends LitElement {
         minimumFractionDigits: 2
       })
 
+      let status = '';
+      let name = '';
+      switch (card.status) {
+        case 'BLOCKED':
+          status = 'blocked';
+          name = 'bloqueada'
+          break;
+
+        case 'INOPERATIVE':
+          status = 'off'
+          name = 'inoperativa'
+          break;
+
+        case 'CANCELED':
+          status = 'off'
+          name = 'cancelada'
+          break;
+
+        case 'PENDING_EMBOSSING':
+          status = 'off'
+          name = 'grabado'
+          break;
+
+        case 'PENDING_DELIVERY':
+          status = 'off'
+          name = 'en envio'
+          break;
+
+        case 'PRE_ACTIVATED':
+          status = 'off'
+          name = 'por activar'
+          break;
+
+        case 'PENDING_CANCELLATION':
+          status = 'off'
+          name = 'Cancelacion pendiente'
+          break;
+
+        default:
+          status = 'off'
+          name = 'Indefinido'
+          break;
+      }
+
 
       return html`
-        <bbva-list-card class="card" variant="card" show-card id=${card.cardId} card-status=${card.status}
-          card-title=${card.name} num-product=${card.cardNumber} amount=${card.padingBalance.amount}
-          currency-code=${card.padingBalance.currency} content-text=${this.currenciesBalancePedding}
-          credit-balance=${formatter.format(card.currentBalance.amount)} secondary-currency-code=${card.currentBalance.currency}
-          content-text-extra=${this.currenciesBalanceAvalible} card-image=${imagesList.find(item=> item.id
-          === card.brand).link}
+        <bbva-list-card 
+          class="card" 
+          variant="card" 
+          show-card id=${card.cardId} 
+          card-status=${status} 
+          card-title=${card.name}
+          num-product=${card.cardNumber} 
+          amount=${card.padingBalance.amount} 
+          currency-code=${card.padingBalance.currency}
+          content-text=${this.currenciesBalancePedding} 
+          credit-balance=${formatter.format(card.currentBalance.amount)}
+          secondary-currency-code=${card.currentBalance.currency} 
+          content-text-extra=${this.currenciesBalanceAvalible}
+          card-image=${imagesList.find(item=> item.id === card.brand).link}
+          badge-text=${name}
+          badge-type=${status}
           @click=${this.cardClick}
+        
           >
         </bbva-list-card>
         `
